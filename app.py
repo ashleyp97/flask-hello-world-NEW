a9bde99fbcacc45b73e19bfcd2528102961eb381
Ash <ashleypan9901@gmail.com>
2025-07-26 09:13:17 -0400

    Db create made

--- app.py
+++ app.py
@@ -10,4 +10,21 @@ def hello_world():
 def db_test():
     conn = psycopg2.connect("postgresql://lab_10_qg0a_user:RLFv1qvFiD42BaJ4enaGrPxVYio9Ndna@dpg-d22cn8fgi27c73eqh7sg-a/lab_10_qg0a")
     conn.close()
-    return 'db_test working'
+    return 'db test working'
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://lab_10_qg0a_user:RLFv1qvFiD42BaJ4enaGrPxVYio9Ndna@dpg-d22cn8fgi27c73eqh7sg-a/lab_10_qg0a")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    cur.close()
+    return "basketball table created"
